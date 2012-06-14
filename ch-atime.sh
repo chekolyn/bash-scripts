545ec1cfb5bea7c80e3cce4ed9ed6340ef7bb269
Sergio Aguilar <chekolyn@gmail.com>
2012-06-13 19:00:31 -0700

    Adding ch-atime

---
+++ ch-atime.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -   
+#title          :ch-atime.sh
+#description    :Will change access time of multiple files
+#author         :Sergio Aguilar
+#date           :20120613
+#version        :0.1    
+#usage          :./ch-atime.sh
+#notes          :       
+#bash_version   :4.2.24(1)-release
+#============================================================================
+
+echo "Enter extention of file you want to change:"
+read EXT
+
+DIR=$(pwd)
+echo $DIR
+
+for file in $DIR/*.$EXT
+do
+  #Get the 
+  TIME=$(stat -c%y ${file})
+  echo "Changing $file"
+  stat $file
+  
+  #Changing access time to modify time (created)
+  touch $file -a -d "$TIME"
+  stat $file
+done
