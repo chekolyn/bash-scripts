070caf9bd556847455b4ca3b3155abb54a9b0144
Sergio Aguilar <chekolyn@gmail.com>
2013-04-17 12:59:07 -0700

    Updating audit-log-back

--- audit-log-back.sh
+++ audit-log-back.sh
@@ -2,23 +2,30 @@
 #title          :audit-log-back.sh
 #description    :Backups audit files into a tar file
 #author         :Sergio Aguilar
-#date           :20121119
-#version        :0.1    
+#date           :2012-04-17
+#version        :0.1.1
 #usage          :./audit-back.sh
 #notes          :       
 #bash_version   :4.2.24(1)-release
 #============================================================================
 
 LOG_FOLDER=/var/log/audit
-NOW=$(date +"%F--%T")
-FILE=$LOG_FOLDER/audits-$NOW.tar.gz
+NOW=$(date +"%F")
+HOST=$(hostname)
+FILE=$LOG_FOLDER/audits-$HOST-$NOW.tar.gz
 
 # Delete previous tar files:
 find $LOG_FOLDER -name "*.tar.gz" -exec rm {} \;
 
-# Find files and create tar file
-# it will find logs file older than a day (-mtime +1)
-find $LOG_FOLDER -name "*.log.*" -mtime +1  | xargs tar cvfz $FILE
+# First look for old log files:
+if [ "$(ls -A *.log.* 2> /dev/null)" ]  ; then
+  echo "Old log files found; creating tar..."
+  # Find old log files and create tar file
+  # it will find logs file older than a day (-mtime +1)
+  find $LOG_FOLDER -name "*.log.*" -mtime +1  | xargs tar cvfz $FILE
+else
+  echo "No old logs files found"
+fi
 
 # Find files and delete the files:
 find $LOG_FOLDER -name "*.log.*" -mtime +1  -exec rm {} \;
